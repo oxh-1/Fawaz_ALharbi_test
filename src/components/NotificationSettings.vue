fe637636a89723af4019da7a55957a62c0e39ce1
FAWAZ <144858110+oxh-1@users.noreply.github.com>
2026-03-24 01:50:38 +0300

    Remove username assignment in created lifecycle hook
    Removed assignment of username from loggedInUser.

--- src/components/NotificationSettings.vue
+++ src/components/NotificationSettings.vue
@@ -174,9 +174,7 @@ export default {
     const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
     if (loggedInUser) {
       // Check for .name (Google) OR .username (Manual)
-      this.username = loggedInUser.name || loggedInUser.username || 'User';
     }
-    this.checkSystemNotification();
   },
   computed: {
     ...mapState(['isDarkMode'])
