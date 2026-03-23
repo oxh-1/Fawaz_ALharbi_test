7c7df022d317fdad092a7ea6c8367f31722d80ad
FAWAZ <144858110+oxh-1@users.noreply.github.com>
2026-03-24 01:08:28 +0300

    Change Google login button to submit type

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -38,7 +38,7 @@
         <button type="submit">{{ $t('login.loginButton') }}</button>
         <router-link to="/forgot-password" class="forgot-password">{{ $t('login.forgotPassword') }}</router-link>
           <div id="google-btn">
-        <button class="google-login">
+        <button type="submit"class="google-login">
           <img src="@/assets/Gittax/google.png" alt="Google Logo" class="google-icon" />
           {{ $t('login.googleLogin') }}
         </button></div>
